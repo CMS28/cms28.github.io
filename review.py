cee68b9904e9e092bb3e770afb5fd792137e25b3
CMS28 <54415283+CMS28@users.noreply.github.com>
2019-12-05 12:56:18 -0500

    Add files via upload

--- review.py
+++ review.py
@@ -33,7 +33,7 @@ print(f'1 cup of Whole milk has {mcal} Calories.')
 print(f'60 gms of Paneer has {pcal} Calories.')
 print(f'1 tbsp of Butter has {bcal} Calories')
 '''
-
+'''
 class rectangle:
     def __init__(self, width, height):
         self.width = width
@@ -43,3 +43,22 @@ class rectangle:
 A = rectangle(4,5)
 A.area()
 print(f'{A.area()}')
+'''
+def RecPascal(n, m=1, prev=[]):
+    if m > n+1:
+        return []
+    elif m == 1:
+        return RecPascal(n, m+1 , [1])
+    else:
+        return [prev] + RecPascal(n, m+1, calculate(prev))
+
+def calculate(prev):
+    res = [0]*(len(prev)+1)
+    res[0], res[-1] = 1, 1
+    for i in range(0,len(res)):
+        if res[i] == 0:
+            res[i] = prev[i-1] + prev[i]
+    return res
+
+for line in RecPascal(8):
+    print(line)
